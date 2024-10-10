de7ecb56054d1175c2983e62d55231c70aaf9bc6
Esteban Solarte <estebansolarte86@gmail.com>
2024-10-10 18:08:56 -0500

    refactor: The task entity is created by an object

--- src/task/domain/entity/task.entity.ts
+++ src/task/domain/entity/task.entity.ts
@@ -1,12 +1,30 @@
 import { TaskID } from './props/taskID';
 import { TaskStatus } from './props/taskStatus';
 
+interface TaskProps {
+  id: TaskID;
+  description: string;
+  status: TaskStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 export class Task {
-  constructor(
-    readonly id: TaskID,
-    readonly description: string,
-    readonly status: TaskStatus,
-    readonly createdAt: Date,
-    readonly updatedAt: Date,
-  ) {}
+  readonly id: TaskID;
+
+  readonly description: string;
+
+  readonly status: TaskStatus;
+
+  readonly createdAt: Date;
+
+  readonly updatedAt: Date;
+
+  constructor({ id, description, status, createdAt, updatedAt }: TaskProps) {
+    this.id = id;
+    this.description = description;
+    this.status = status;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
 }
